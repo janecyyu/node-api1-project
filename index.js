c78bb19010f5247a118f903b29bf00ddc7595dab
janecyyu <janecyyu@gmail.com>
2020-05-04 14:31:23 -0700

    POST finished

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const express = require("express");
 
 const server = express();
+server.use(express.json()); //teaches express how to read json from the body
 
-let users = [];
+let userList = [];
 // server.get('/', (req, res) => {
 //   res.send('Hello World');
 // });
 
 server.post("/api/users", (req, res) => {
+    const newUser = req.body;
+
   //If the request body is missing the name or bio property:
-  if (req.hasOwnProperty("name") || req.hasOwnProperty("bio")) {
+  if (newUser.name === undefined || newUser.bio === undefined) {
     res.status(400).send("Please provide name and bio for the user.");
+  } else {
+    //If the information about the user is valid:
+    userList.push(newUser);
+    res.status(201).send(newUser);
   }
-  //If the information about the user is valid:
-  const newUser = req.body;
-  users.push(newUser);
-  res.status(201).send(newUser);
   //If there's an error while saving the user:
-  if(){
-    res.status(500).send("There was an error while saving the user to the database");
+  if (userList.hasOwnProperty(newUser)) {
+    res
+      .status(500)
+      .send("There was an error while saving the user to the database");
   }
 });
 
