a6e9099a018098c3628882899e0f6e6f8bcea880
janecyyu <janecyyu@gmail.com>
2020-05-04 15:11:56 -0700

    DELETE done

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const server = express();
 server.use(express.json()); //teaches express how to read json from the body
 
 let userList = [
-    {
-        id: 1, // hint: use the shortid npm package to generate it
-        name: "Jane Doe", // String, required
-        bio: "Not Tarzan's Wife, another Jane",  // String, required
-      }
+  {
+    id: 1, // hint: use the shortid npm package to generate it
+    name: "Jane Doe", // String, required
+    bio: "Not Tarzan's Wife, another Jane", // String, required
+  },
 ];
 // server.get('/', (req, res) => {
 //   res.send('Hello World');
@@ -45,10 +45,30 @@ server.get("/api/users/:id", (req, res) => {
   const id = Number(req.params.id);
   const getUser = userList.filter((user) => user.id === id);
   //if the user with the specified id is not found:
-  if(getUser.length<=0){
+  if (getUser.length <= 0) {
     res.status(404).send("The user information could not be retrieved.");
-  }else{
-      res.status(200).send(getUser)
+  } else {
+    res.status(200).send(getUser);
+  }
+  //If there's an error in retrieving the user from the database:
+  if (getUser === undefined) {
+    res.status(500).send("The user information could not be retrieved.");
+  }
+});
+
+server.delete("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const getUser = userList.filter((user) => user.id === id);
+  //if the user with the specified id is not found:
+  if (getUser.length <= 0) {
+    res.status(404).send("The user with the specified ID does not exist.");
+  } else {
+    userList = userList.filter((user) => user.id !== id);
+    res.status(200).send(userList);
+  }
+  //If there's an error in removing the user from the database:
+  if (userList.hasOwnProperty(getUser)) {
+    res.status(500).send("The user could not be removed");
   }
 });
 
